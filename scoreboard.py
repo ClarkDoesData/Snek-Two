2aa8d60f63bc09c49dc2b41388440cfbfd522218
Clark Montano <clrk.montano@gmail.com>
2023-04-09 15:00:38 +0800

    X

--- scoreboard.py
+++ scoreboard.py
@@ -9,22 +9,28 @@ class Scoreboard(Turtle):
         self.penup()
         self.color("white")
         self.goto(0, 265)
+        self.high_score = 0
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        self.clear()
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.color("white")
-        self.hideturtle()
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     self.color("white")
+    #     self.hideturtle()
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self. score = 0
+        self.update_scoreboard()
 
 
